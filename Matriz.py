a4e0570c12f6a0886d5bb815c74e4f70bafcb69c
Gabriel Rosatto Dantas <105675641+Rosatto@users.noreply.github.com>
2024-01-07 15:24:56 -0300

    Create Matriz.py

---
+++ Matriz.py
@@ -0,0 +1,15 @@
+'''Desafios 🥇
+
+Desafio #1 Crie uma lista que tenha os nomes dos 3 objetos que você mais usa durante o dia e imprima ele na tela
+Desafio #2 Usando apenas uma linha de código, crie uma lista de 10 a 131
+Desafio #3 Imprima na tela o resultado da combinação da lista do desafio 1 e desafio 2
+Desafio #4 Crie uma lista de listas(matriz) que tenha os nomes dos 3 objetos que você mais usa durante o dia, mas agora dentro de cada item você vai colocar uma informação extra, coloque o valor em reais desse objeto também e imprima ele na tela'''
+
+lista_objetos = ['mouse','teclado','headset']
+print(lista_objetos)
+
+lista_numeros = list(range(10,132))
+print(lista_objetos + lista_numeros)
+
+matriz_objetos = [['teclado', 10],['mouse', 20],['heaset', 30]]
+print(matriz_objetos[2][0])
